18da57be255053ab5014fd7bb17bcbf96f6b6125
ekwan <224302@nttdata.com>
2021-09-30 11:30:32 -0700

    add configuration tests

--- src/main/java/ca/bc/gov/open/Scss/Controllers/FileController.java
+++ src/main/java/ca/bc/gov/open/Scss/Controllers/FileController.java
@@ -149,7 +149,7 @@ public class FileController {
                             HttpMethod.GET,
                             new HttpEntity<>(new HttpHeaders()),
                             FileNumbeSearchPublicAccessResponse.class);
-            if(resp.getBody() != null && (resp.getBody().getCourtFiles() != null || resp.getBody().getCourtFiles().size() == 0)){
+            if(resp.getBody() != null && (resp.getBody().getCourtFiles() == null || resp.getBody().getCourtFiles().size() == 0)){
                 resp.getBody().setCourtFiles(null);
             }
             return resp.getBody();
