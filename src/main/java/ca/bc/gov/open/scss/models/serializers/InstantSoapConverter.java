28c9b38b1036ba092da4599b408351a5185b9a26
ekwan <224302@nttdata.com>
2021-10-21 10:23:22 -0700

    cahnge date format

--- src/main/java/ca/bc/gov/open/scss/models/serializers/InstantSoapConverter.java
+++ src/main/java/ca/bc/gov/open/scss/models/serializers/InstantSoapConverter.java
@@ -1,13 +1,12 @@
 package ca.bc.gov.open.scss.models.serializers;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public final class InstantSoapConverter {
@@ -25,12 +24,12 @@ public final class InstantSoapConverter {
             // Try to parse a datetime first then try date only if both fail return null
             try {
                 // Date time parser
-                var sdf = new SimpleDateFormat("dd-MMM-yy hh.mm.ss.SSSSSS a", Locale.US);
+                var sdf = new SimpleDateFormat("yyyy-MM-dd hh.mm.ss.SSSSSS a", Locale.US);
                 sdf.setTimeZone(TimeZone.getTimeZone("GMT-7"));
                 d = sdf.parse(value);
             } catch (ParseException ex) {
                 // Date only parser
-                var sdf = new SimpleDateFormat("dd-MMM-yy", Locale.US);
+                var sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
                 sdf.setTimeZone(TimeZone.getTimeZone("GMT-7"));
                 d = sdf.parse(value);
             }
