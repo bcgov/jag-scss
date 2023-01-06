8f8876c23c82f85342be0b8ddbd0ff39169b8778
jianming tu <tu_jm@yahoo.com>
2023-01-06 12:51:43 -0800

    remove unused file

--- src/main/java/ca/bc/gov/open/scss/configuration/ORDSHttpHeaders.java
+++
@@ -1,9 +0,0 @@
-package ca.bc.gov.open.scss.configuration;
-
-import org.springframework.http.HttpHeaders;
-
-public class ORDSHttpHeaders extends HttpHeaders {
-    public ORDSHttpHeaders() {
-        super();
-    }
-}
